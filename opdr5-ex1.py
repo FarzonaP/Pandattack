33237918fd5374a98ef705767868d8a240b017c8
FarzonaP <84909269+FarzonaP@users.noreply.github.com>
2021-05-27 16:15:42 +0200

    Add files via upload

---
+++ opdr5-ex1.py
@@ -0,0 +1,60 @@
+#Use the input() function to ask the user of your script for their name. If the name they input is your name, print a personalized welcome message. If not, print a different personalized message.
+
+
+
+# a = "Aous"
+
+# name = input('Please input your name:')
+# if name == a:
+#     print('Hello! Welcome, ' + name + '!')
+
+# else:
+#     print('You are ' + name + ', not ' + a + '. Please leave.')
+
+ 
+
+# a = " Farzona"
+
+# name = input("Please input your name:")
+# if name == a:
+#     print("Hello! Welcome ," + name + "!")
+# else:
+#     print("You are " + name + ", , not " + a + " . Please leave .")
+
+
+
+
+
+
+
+b = "Coen"
+
+name = input("Please input your name:")
+if name == b:
+    print("Hello," + b + " You are welcome !") 
+else:
+    print("You are not "   + b +  " you are " + name +  " Please, leave ") 
+
+
+
+    
+
+    
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
